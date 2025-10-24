a3bad2e1783deff22627c926ff707b31cebdb178
Kushal <144584446+Yato-22@users.noreply.github.com>
2025-10-24 13:59:04 +0530

    Add vehicle mileage calculation program

---
+++ code.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(){
+    char name[10][20];
+    int distance, fuel, num_veh, i = 0;
+    float mileage[10];
+
+    invalid1:
+    printf("Enter number of vehicles: ");
+    scanf("%d", &num_veh);
+    printf("\n");
+    if(num_veh > 10){
+        printf("Number of vehicles cannot be more than 10. \n");
+        goto invalid1;
+    }
+
+    while(i < num_veh){
+        printf("Enter the name of the vehicle: ");
+        scanf("%s", &name[i]);
+        printf("Enter the distance traveled (km): ");
+        scanf("%d", &distance);
+        printf("Enter the fuel consumed (litre): ");
+        scanf("%d", &fuel);
+        printf("\n");
+
+        mileage[i] = distance / fuel;
+        i++;
+    }
+    i = 0;
+
+    printf("---- Vehicle Efficiency Report ---");
+    printf("\n %-12s %-16s %-12s", "Vehicle", "Mileage(km/l)", "Efficiency");
+    while(i < num_veh){
+        if(mileage[i]>20){
+            printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "High Efficiency");
+        }
+        else if(mileage[i] >= 15){
+            printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Moderate Efficiency");
+        }
+        else if(mileage[i] >= 10){
+            printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Low Efficiency");
+        }
+        else{
+            printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Poor Efficiency");
+        }
+        i++;
+    }
+
+    return 0;
+}
