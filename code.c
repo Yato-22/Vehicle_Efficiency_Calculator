919973c7e9258702a40f8d4e2181871262a02629
Kushal <144584446+Yato-22@users.noreply.github.com>
2025-10-27 16:09:48 +0530

    Add vehicle type handling and efficiency reporting

--- code.c
+++ code.c
@@ -2,7 +2,7 @@
 
 int main(){
     char name[10][20];
-    int distance, fuel, num_veh, i = 0;
+    int distance, fuel, num_veh, i = 0, type[10];
     float mileage[10];
 
     invalid1:
@@ -15,6 +15,8 @@ int main(){
     }
 
     while(i < num_veh){
+        printf("Enter the type of the vehicle, 1 for bike, 2 for car: ");
+        scanf("%s", &type[i]);
         printf("Enter the name of the vehicle: ");
         scanf("%s", &name[i]);
         printf("Enter the distance traveled (km): ");
@@ -31,17 +33,33 @@ int main(){
     printf("---- Vehicle Efficiency Report ---");
     printf("\n %-12s %-16s %-12s", "Vehicle", "Mileage(km/l)", "Efficiency");
     while(i < num_veh){
-        if(mileage[i]>20){
-            printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "High Efficiency");
-        }
-        else if(mileage[i] >= 15){
-            printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Moderate Efficiency");
-        }
-        else if(mileage[i] >= 10){
-            printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Low Efficiency");
+        if(type[i] == 2){
+            if(mileage[i]>20){
+                printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "High Efficiency");
+            }
+            else if(mileage[i] >= 15){
+                printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Moderate Efficiency");
+            }
+            else if(mileage[i] >= 10){
+                printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Low Efficiency");
+            }
+            else{
+                printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Poor Efficiency");
+            }
         }
         else{
-            printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Poor Efficiency");
+            if(mileage[i]>35){
+                printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "High Efficiency");
+            }
+            else if(mileage[i] >= 25){
+                printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Moderate Efficiency");
+            }
+            else if(mileage[i] >= 15){
+                printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Low Efficiency");
+            }
+            else{
+                printf("\n %-12s %-16.1f %-12s", name[i], mileage[i], "Poor Efficiency");
+            }
         }
         i++;
     }
